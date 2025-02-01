d59765e637e84eb559a68b40b191f2058a5e75f1
developer111222 <anishprajapati765@gmail.com>
2025-02-01 10:43:03 +0530

    second

--- database/Db.js
+++ database/Db.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 // Connect to MongoDB
 const dbconnect = () => {
 
-    //mongodb+srv://jiyakhurana68:EJEYD0dONF5Fa7HP@cluster0.7qewb.mongodb.net/
 
-    mongoose.connect(" mongodb+srv://jiyakhurana68:EJEYD0dONF5Fa7HP@cluster0.7qewb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect(process.env.MONGO_URL)
    
         .then(() => console.log("MongoDB Connected..."))
         .catch(err => console.error("MongoDB connection error:", err));
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors=require('cors');
 const bd=require('./database/Db')
 const app = express();
 const apiroute=require('./router/apirouter');
+require('dotenv').config()
+
 
 app.use(express.json());
 app.use(cors());
@@ -24,6 +26,6 @@ app.use(cors(corsoptions));
 app.use('/api',apiroute);
 
 //server
-const port=5000;
+const port=process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));``
