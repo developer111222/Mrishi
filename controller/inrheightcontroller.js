449b14e7c0228056e9ccc5f357caadd9f976bdce
developer111222 <61453529+developer111222@users.noreply.github.com>
2025-03-06 16:29:50 +0530

    Create inrheightcontroller.js

---
+++ controller/inrheightcontroller.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const Inrheight=require('../model/inrheight');
+
+exports.apicontroller=async (req,res)=>{
+    try{
+const data=req.body;
+
+
+if(!data){
+    return res.status(400).json({message:'No data provided'});
+}
+
+const api=new Inrheight(data);
+await api.save();
+
+return res.status(201).json({message:'API created successfully',api});
+
+    }catch(error){
+        return res.status(500).json({message:error.message});
+    }
+
+}
+
+
+//---------------------------get ----------------------
+
+exports.getApis=async (req,res)=>{
+    try{
+        const apis=await Inrheight.find();
+        return res.status(200).json({success:true,apis});
+    }catch(error){
+        return res.status(500).json({message:error.message});
+    }
+}
