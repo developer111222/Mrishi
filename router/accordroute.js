f93f4a5069ef8c752e12073e4b4a4aaa816f8240
developer111222 <61453529+developer111222@users.noreply.github.com>
2025-03-22 11:25:38 +0530

    Update accordroute.js

--- router/accordroute.js
+++ router/accordroute.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router=express.Router();
-const {apicontroller,getApis}=require('../controller/accordcontroller');
+const {accordcontroller,getaccord}=require('../controller/accordcontroller');
 
-router.route('/form').post(apicontroller);
-router.route('/get-db').get(getApis);
+router.route('/form').post(accordcontroller);
+router.route('/get-db').get(getaccord);
 
 module.exports=router;
