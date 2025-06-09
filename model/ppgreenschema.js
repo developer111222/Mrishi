ad97bb481d8d0f7f74b043ad033d08297f369f33
developer111222 <61453529+developer111222@users.noreply.github.com>
2025-06-09 11:50:43 +0530

    Update ppgreenschema.js

--- model/ppgreenschema.js
+++ model/ppgreenschema.js
@@ -1,1 +1,23 @@
+const mongoose = require('mongoose');
 
+const ppgreenschema= new mongoose.Schema({
+    name:{
+        type:String,
+        // required:true
+    },
+    email:{
+        type:String,
+        // required:true
+    },
+    phone:{
+        type:Number,
+        // required:true
+    },
+ 
+    created_at: { type: Date, default: Date.now },
+  
+});
+
+const PPGreen = mongoose.model('Accord', ppgreenschema);
+
+module.exports = PPGreen;
