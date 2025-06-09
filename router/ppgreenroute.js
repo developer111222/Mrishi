39d1cd4fe68b9919311c0eb72a2937e0b75b9935
developer111222 <61453529+developer111222@users.noreply.github.com>
2025-06-09 14:57:52 +0530

    Update ppgreenroute.js

--- router/ppgreenroute.js
+++ router/ppgreenroute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router=express.Router();
 const {apicontroller,getApis}=require('../controller/ppgreencontroller');
 
-router.route('/form').post(apicontroller);
-router.route('/get-db').get(getApis);
+router.route('/pp-form').post(apicontroller);
+router.route('/pp-get-db').get(getApis);
 
 module.exports=router;
